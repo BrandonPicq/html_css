1a7aa10f5ebff947ec0e906563620786b59d9af6
nybi <nybi@MacBook-Air-de-nybi.local>
2025-12-18 10:40:09 +0100

    [UPDATE]: Make the navbar sticky to always be visible

--- script.js
+++ script.js
@@ -1,20 +1,18 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const navbarToggle = document.querySelector(".navbar-toggle");
-  const navbarMenu = document.querySelector(".navbar-menu");
-  const navbarActions = document.querySelector(".navbar-actions");
+const navbarToggle = document.querySelector(".navbar-toggle");
+const navbarMenu = document.querySelector(".navbar-menu");
+const navbarActions = document.querySelector(".navbar-actions");
 
-  navbarToggle.addEventListener("click", function () {
-    navbarMenu.classList.toggle("active");
-    navbarActions.classList.toggle("active");
-    navbarToggle.classList.toggle("active");
-  });
+navbarToggle.addEventListener("click", function () {
+  navbarMenu.classList.toggle("active");
+  navbarActions.classList.toggle("active");
+  navbarToggle.classList.toggle("active");
+});
 
-  const navbarLinks = document.querySelectorAll(".navbar-link");
-  navbarLinks.forEach((link) => {
-    link.addEventListener("click", function () {
-      navbarMenu.classList.remove("active");
-      navbarActions.classList.remove("active");
-      navbarToggle.classList.remove("active");
-    });
+const navbarLinks = document.querySelectorAll(".navbar-link");
+navbarLinks.forEach((link) => {
+  link.addEventListener("click", function () {
+    navbarMenu.classList.remove("active");
+    navbarActions.classList.remove("active");
+    navbarToggle.classList.remove("active");
   });
 });
