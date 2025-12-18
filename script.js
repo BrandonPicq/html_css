80e2b36466f79f423f056a01b68eb2a31c863c71
nybi <nybi@MacBook-Air-de-nybi.local>
2025-12-18 10:28:30 +0100

    [ADD] [BONUS]: Make the burger button do something

---
+++ script.js
@@ -0,0 +1,20 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const navbarToggle = document.querySelector(".navbar-toggle");
+  const navbarMenu = document.querySelector(".navbar-menu");
+  const navbarActions = document.querySelector(".navbar-actions");
+
+  navbarToggle.addEventListener("click", function () {
+    navbarMenu.classList.toggle("active");
+    navbarActions.classList.toggle("active");
+    navbarToggle.classList.toggle("active");
+  });
+
+  const navbarLinks = document.querySelectorAll(".navbar-link");
+  navbarLinks.forEach((link) => {
+    link.addEventListener("click", function () {
+      navbarMenu.classList.remove("active");
+      navbarActions.classList.remove("active");
+      navbarToggle.classList.remove("active");
+    });
+  });
+});
